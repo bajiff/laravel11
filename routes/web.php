cb8a98e44819456721ab6d8ff6da55358d197a35
MrBaji <mr.baji.smart@gmail.com>
2025-03-28 23:28:08 +0700

    Belajar Blade Component

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['nama' => 'Mekikun :D']);
+    return view('home', ['title' => 'Home Page']);
 });
 
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Baji Ajalah']);
+    return view('about', ['title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['instagram' => 'https://instagram.com/_bajif', 'github' => 'https://github.com/bajiff/_bajiff']);
+    return view('contact', ['title' => 'Contact', 'instagram' => 'https://instagram.com/_bajif', 'github' => 'https://github.com/bajiff/_bajiff']);
 });
