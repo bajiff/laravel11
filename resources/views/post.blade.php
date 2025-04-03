35667276ac3ac81a082da117cadd5eea127c62a6
MrBaji <mr.baji.smart@gmail.com>
2025-04-03 20:39:45 +0700

    Amburadul, setelah kembali ke branch sebelum nya dan mengulangi pelajaran malah disuruh ngemerge, bisa buat pelajaran ini

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-  
+ 
   <article class="py-8 max-w-screen-md ">
       <h1 class="mb-1 tracking-tight text-3xl font-semibold text-gray-900">{{ $post['title'] }}</h1>
     <div class="my-3 text-gray-700">
@@ -9,6 +9,4 @@
     <p class="my-4 font-light">{{ $post['body'] }}</p>
     <a href="/posts" class="text-blue-500 font-medium hover:underline ease-in-out transition-all duration-300">&laquo; Back to Blog</a>
   </article>
-
-
 </x-layout>
