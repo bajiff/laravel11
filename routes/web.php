565fd68feb6b58ebdd4b03dd4069beaf1f588625
MrBaji <mr.baji.smart@gmail.com>
2025-04-05 15:22:26 +0700

    Telah mempelajari Database & Migration, Lanjut ke Eloquent ORM, Post Model

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,22 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home Page']);
+    return view(
+        'home',
+        [
+            'title' => 'Home Page'
+        ]
+    );
 });
 
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'About']);
+    return view(
+        'about',
+        [
+            'title' => 'About'
+        ]
+    );
 });
 
 Route::get('/posts', function () {
@@ -35,6 +45,16 @@ Route::get('/posts/{slug}', function ($slug) {
     );
 });
 
-Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact', 'instagram' => 'https://instagram.com/_bajif', 'github' => 'https://github.com/bajiff/_bajiff']);
-});
+Route::get(
+    '/contact',
+    function () {
+        return view(
+            'contact',
+            [
+                'title' => 'Contact',
+                'instagram' => 'https://instagram.com/_bajif',
+                'github' => 'https://github.com/bajiff/_bajiff'
+            ]
+        );
+    }
+);
