c8499b689e040f2c95f0d8acebb4de4ca4f8be4e
MrBaji <mr.baji.smart@gmail.com>
2025-04-07 20:02:25 +0700

    Telah mempelajari Model Factories, lanjut ke Eloquent Relationship

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use PhpParser\Node\Expr\Cast\Bool_;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -27,7 +28,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('cobain'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -41,4 +42,5 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
 }
