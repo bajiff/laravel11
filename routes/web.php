e55c41afbb622b59c7630f307fe4a09001a87623
MrBaji <mr.baji.smart@gmail.com>
2025-04-12 18:33:28 +0700

    Telah mempelajari N + 1 Problem, lanjut ke Redesign UI

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
     return view(
         'posts',
         [
             'title' => 'Blog',
-            'posts' => Post::all()
+            'posts' => $posts,
         ]
     );
 });
@@ -46,15 +48,17 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     );
 });
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
     return view(
         'posts',
         [
-            'title' => count($user->posts) .' Article by ' . $user->name,
+            'title' => count($user->posts) . ' Article by ' . $user->name,
             'posts' => $user->posts
         ]
     );
 });
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('author', 'category');
     return view(
         'posts',
         [
