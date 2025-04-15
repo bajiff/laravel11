ae0b4c3170a17b5c3de0f6b8eeaafed4f3887d6d
MrBaji <mr.baji.smart@gmail.com>
2025-04-15 14:59:49 +0700

    Menambahkan serta memodifikasi modified:   package-lock.json         modified:   package.json         new file:   postcss.config.js         new file:   tailwind.config.js         modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    darkMode: 'class',
-    theme: {
-        extends: {
-            colors: {
-                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
-            },
-        },
-        fontFamily: {
-            'body': [
-                'Inter',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'system-ui',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ],
-            'sans': [
-                'Inter',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'system-ui',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ],
-        },
-    },
     plugins: [
-        require("flowbite/plugin"),
-        require('flowbite-typography'),
-
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-
-    safelist: [
-        "bg-red-100",
-        "bg-yellow-100",
-        "bg-green-100",
-        "bg-blue-100",
-        "bg-orange-100",
-    ]
 });
